2048be03c1b41b6a0bfbd3a703c2962b7cb3d4af
Kubiran Karakaran <53050664+kuby1412@users.noreply.github.com>
2020-09-09 21:48:18 +0800

    Create load.S

---
+++ Day@2/load.S
@@ -0,0 +1,14 @@
+.section .text
+.global load
+.type load,@function
+
+load:
+	    add a4,a0,zero
+	    add a2,a0,a1
+	    add a3,a0,a1
+	    add a3,a0,zero
+loop:	add a4,a3,a4
+	    addi a3,a2,loop
+	    blt a3,a2,1
+	    add a0,a4,zero
+	    ret.
